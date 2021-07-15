e2f65ae4e3e9f721af4186c8a5ff6c95f1b76d37
Aditya Yadavalli <32356916+AdityaYadavalli1@users.noreply.github.com>
2021-07-15 16:27:09 +0530

    Update path.sh
    Add the binaries needed for DNN recipes.

--- path.sh
+++ path.sh
@@ -1,9 +1,11 @@
-export KALDI_ROOT="/home/user1/kaldi"
-[ -f $KALDI_ROOT/tools/env.sh ] && . $KALDI_ROOT/tools/env.sh
-export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$PWD:$PATH
-[ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
-. $KALDI_ROOT/tools/config/common_path.sh
+# Defining Kaldi root directory
+export KALDI_ROOT="/home/user1/kaldi" #change this when we transfer to server
+# Setting paths to useful tools
+#export PATH=$PWD/utils/:$KALDI_ROOT/src/bin:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/src/fstbin/:$KALDI_ROOT/src/gmmbin/:$KALDI_ROOT/src/featbin/:$KALDI_ROOT/src/lmbin/:$KALDI_ROOT/src/sgmm2bin/:$KALDI_ROOT/src/fgmmbin/:$KALDI_ROOT/src/latbin/:$PWD:$PATH
+# Defining audio data directory (modify it for your installation directory!)
+export PATH=$PWD/utils/:$KALDI_ROOT/src/bin:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/src/fstbin/:$KALDI_ROOT/src/gmmbin/:$KALDI_ROOT/src/featbin/:$KALDI_ROOT/src/lm/:$KALDI_ROOT/src/sgmmbin/:$KALDI_ROOT/src/sgmm2bin/:$KALDI_ROOT/src/fgmmbin/:$KALDI_ROOT/src/latbin/:$KALDI_ROOT/src/nnetbin:$KALDI_ROOT/src/nnet2bin/:$KALDI_ROOT/src/nnet3bin/:$KALDI_ROOT/src/kwsbin:$KALDI_ROOT/src/online2bin/:$KALDI_ROOT/src/ivectorbin/:$KALDI_ROOT/src/lmbin/:$PWD:$PATH
+export DATA_ROOT="/home/user2/Aditya/digits_IIIT" #change this as well, although we are not using it as far as I know
+# Enable SRILM
+. $KALDI_ROOT/tools/env.sh
+# Variable needed for proper data sorting
 export LC_ALL=C
-
-# For now, don't include any of the optional dependenices of the main
-# librispeech recipe
