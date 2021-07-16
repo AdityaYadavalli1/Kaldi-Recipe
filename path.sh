f48085e6104290f3ab8a1b5eed615b6714459787
Aditya Yadavalli <32356916+AdityaYadavalli1@users.noreply.github.com>
2021-07-16 13:58:16 +0530

    Change binaries in path.sh

--- path.sh
+++ path.sh
@@ -1,11 +1,22 @@
-# Defining Kaldi root directory
-export KALDI_ROOT="/home/user1/kaldi" #change this when we transfer to server
-# Setting paths to useful tools
-#export PATH=$PWD/utils/:$KALDI_ROOT/src/bin:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/src/fstbin/:$KALDI_ROOT/src/gmmbin/:$KALDI_ROOT/src/featbin/:$KALDI_ROOT/src/lmbin/:$KALDI_ROOT/src/sgmm2bin/:$KALDI_ROOT/src/fgmmbin/:$KALDI_ROOT/src/latbin/:$PWD:$PATH
-# Defining audio data directory (modify it for your installation directory!)
+# path to kaldi root directory
+export KALDI_ROOT="/home/user1/kaldi"
+
+#all the required binaries for DNN based recipes
 export PATH=$PWD/utils/:$KALDI_ROOT/src/bin:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/src/fstbin/:$KALDI_ROOT/src/gmmbin/:$KALDI_ROOT/src/featbin/:$KALDI_ROOT/src/lm/:$KALDI_ROOT/src/sgmmbin/:$KALDI_ROOT/src/sgmm2bin/:$KALDI_ROOT/src/fgmmbin/:$KALDI_ROOT/src/latbin/:$KALDI_ROOT/src/nnetbin:$KALDI_ROOT/src/nnet2bin/:$KALDI_ROOT/src/nnet3bin/:$KALDI_ROOT/src/kwsbin:$KALDI_ROOT/src/online2bin/:$KALDI_ROOT/src/ivectorbin/:$KALDI_ROOT/src/lmbin/:$PWD:$PATH
-export DATA_ROOT="/home/user2/Aditya/digits_IIIT" #change this as well, although we are not using it as far as I know
-# Enable SRILM
-. $KALDI_ROOT/tools/env.sh
-# Variable needed for proper data sorting
+
+[ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
+. $KALDI_ROOT/tools/config/common_path.sh
+
+
 export LC_ALL=C
+export IRSTLM=$KALDI_ROOT/tools/irstlm
+
+
+# Variable that stores path to MITLM library
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/tools/mitlm-svn/lib
+
+SRILM_ROOT=$KALDI_ROOT/tools/srilm
+SRILM_PATH=$SRILM_ROOT/bin:$SRILM_ROOT/bin/i686-m64
+export PATH=$PATH:$SRILM_PATH
+
+export PATH=${PATH}:${IRSTLM}/bin
